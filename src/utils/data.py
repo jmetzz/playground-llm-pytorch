5a0e5aa49c071ba7a3f8cf56c1c294557da316f7
Jean Metz <4632809+username@users.noreply.github.com>
2024-09-30 20:30:38 +0200

    feat: implement masked attention

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any
 
+import tiktoken
 import torch
 from torch.utils.data import DataLoader, Dataset
 
@@ -8,6 +9,22 @@ from torch.utils.data import DataLoader, Dataset
 def load_text_file(file_path: Path) -> str:
     with file_path.open(mode="r", encoding="utf-8") as f:
         return f.read()
+
+
+def get_encoder_and_batch_iterator(
+    file_path: Path,
+    encoding: str = "gpt2",
+    batch_size: int = 8,
+    stride: int = 1,
+    seq_length: int = 1,
+    shuffle: bool = False,
+) -> tuple:
+    content = load_text_file(file_path)
+    encoder = tiktoken.get_encoding(encoding_name=encoding)
+    data_loader = create_dataloader_v1(
+        text=content, encoder=encoder, batch_size=batch_size, stride=stride, seq_length=seq_length, shuffle=shuffle
+    )
+    return encoder, data_loader
 
 
 class GPTDatasetV1(Dataset):
@@ -33,11 +50,11 @@ def create_dataloader_v1(  # noqa: PLR0913, PLR0917
     text: str,
     encoder: Any,
     batch_size: int = 4,
-    max_length: int = 256,
+    seq_length: int = 256,
     stride: int = 128,
     shuffle: bool = True,
     drop_last: bool = True,
     num_workers: int = 0,
-):
-    dataset = GPTDatasetV1(text, encoder, max_length, stride)
+) -> tuple[Dataset, DataLoader]:
+    dataset = GPTDatasetV1(text, encoder, seq_length, stride)
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
