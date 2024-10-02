7bd9f93f70a4498d75751e4b899d710d1f269a65
Jean Metz <4632809+username@users.noreply.github.com>
2024-10-02 17:38:07 +0200

    feat(transformer): add normalization layer

--- src/transformer_cli.py
+++ src/transformer_cli.py
@@ -7,6 +7,7 @@ import torch
 import typer
 
 from transformer.attention import MultiHeadCrossAttention, MultiHeadSelfAttention
+from transformer.normalization import NormalizationLayer
 
 colorama.init(autoreset=True)
 
@@ -17,8 +18,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 @app.command()
-def attention_multihead(batch_size: int = 30, sequence_len: int = 5, dimension: int = 512):
-    token_embeddings = torch.randn((batch_size, sequence_len, dimension))
+def attention_multihead(batch_size: int = 30, seq_length: int = 5, embeddings_dim: int = 32):
+    token_embeddings = torch.randn((batch_size, seq_length, embeddings_dim))
 
     model = MultiHeadSelfAttention(model_dim=512, num_heads=8)
     out = model.forward(token_embeddings)
@@ -26,12 +27,19 @@ def attention_multihead(batch_size: int = 30, sequence_len: int = 5, dimension: 
 
 
 @app.command()
-def attention_multihead_cross(batch_size: int = 30, sequence_len: int = 5, dimension: int = 512):
-    token_embeddings = torch.randn((batch_size, sequence_len, dimension))
+def attention_multihead_cross(batch_size: int = 30, seq_length: int = 5, embeddings_dim: int = 32):
+    token_embeddings = torch.randn((batch_size, seq_length, embeddings_dim))
 
     model = MultiHeadCrossAttention(model_dim=512, num_heads=8)
     out = model.forward(token_embeddings)
     print(out)
+
+
+@app.command()
+def norm_layer(batch_size: int = 30, seq_length: int = 5, embeddings_dim: int = 32):
+    embedding = torch.randn(batch_size, seq_length, embeddings_dim)
+    layer_norm = NormalizationLayer(embeddings_dim)
+    print(layer_norm(embedding))
 
 
 if __name__ == "__main__":
